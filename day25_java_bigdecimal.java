56aebda6a3dc332fae407c60cfeb2fe903d75017
nurpadila_d0221031_c <99775650+nurpadilaparham@users.noreply.github.com>
2022-03-15 19:47:10 +0700

    Add files via upload

---
+++ day25_java_bigdecimal.java
@@ -0,0 +1,28 @@
+import java.math.BigDecimal;
+import java.util.*;
+class Solution{
+
+    public static void main(String []args){
+        //Input
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=sc.next();
+        }
+      	sc.close();
+
+
+        //Write your code here
+        Comparator<String> komparator = new Comparator<String>(){
+            @Override
+            public int compare(String s1, String s2){
+                BigDecimal a = new BigDecimal(s1);
+                BigDecimal b = new BigDecimal(s2);
+                return b.compareTo(a);//membandingkan
+            }
+        };
+        Arrays.sort(s, 0, n, komparator);
+
+
+        //Output
