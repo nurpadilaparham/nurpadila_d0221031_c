5b3c6a60974a0b8df63a42a246c9fe8488443b42
nurpadila_d0221031_c <99775650+nurpadilaparham@users.noreply.github.com>
2022-03-28 21:43:47 +0700

    Add files via upload

---
+++ day38_java_comparator.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+// Write your Checker class here
+class Checker implements Comparator<Player>{
+
+public int compare(Player a, Player b){
+        int compareName = a.name.compareTo(b.name);
+        int compareScore = b.score - a.score;
+        
+        if(compareScore==0){
+            return ((compareName==0)?compareScore:compareName);
+        }else{
+            return compareScore;
+        }
+    }
+}
+
+class Player{
+    String name;
+    int score;
+    
+    Player(String name, int score){
+        this.name = name;
+        this.score = score;
+    }
+}
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+        
+        for(int i = 0; i < n; i++){
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+     
+        Arrays.sort(player, checker);
+        for(int i = 0; i < player.length; i++){
+            System.out.printf("%s %s\n", player[i].name, player[i].score);
+        }
+    }
+}
